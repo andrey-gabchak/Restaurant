d9efb148b8d806129f32ab1eba40bb3f98842482
gabchak <andrey.gabchak@gmail.com>
2017-05-01 21:19:54 +0300

    Added fields and annotation for hibernate

--- src/main/java/ua.goit.restaurant/Model/Employee.java
+++ src/main/java/ua.goit.restaurant/Model/Employee.java
@@ -6,5 +6,29 @@ import java.util.Date;
 @Entity
 @Table(name = "employee")
 public class Employee {
+    @Id
+    @Column(name = "id")
+    private Long id;
 
+    @Column(name = "surname")
+    private String surname;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "DATE_OF_BIRTH")
+    private Date dateOfBirth;
+
+    @Column(name = "phone")
+    private String phoneNumber;
+
+    @Column(name = "salary")
+    private Float salary;
+
+    @Column(name = "JOIN_DATE")
+    private Date joinDate;
+
+    //    @Enumerated(EnumType.STRING) - Взято с примера, но не подходит, так как для позиции выделен отдельный клас. TODO: Переделать.
+    // TODO: Выяснить как передавать связь между классами в DB при помощь hibernate
+    private Position position;
 }
