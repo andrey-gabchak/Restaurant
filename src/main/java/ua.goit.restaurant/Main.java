7310a66892d1b41840956a991c105580f5cce704
gabchak <andrey.gabchak@gmail.com>
2017-05-01 21:17:21 +0300

    Added application-, hibernate-context and empty start method

---
+++ src/main/java/ua.goit.restaurant/Main.java
@@ -0,0 +1,17 @@
+package ua.goit.restaurant;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main {
+
+    public static void main(String[] args) {
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("application-context.xml", "hibernate-context.xml");
+        Main main = applicationContext.getBean(Main.class);
+        main.start();
+    }
+
+    private void start() {
+
+    }
+}
